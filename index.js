7e6b5697f6a4422a358f3387cb514c7f259239f2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-08-22 12:49:32 +0100

    Updated logic for a counter in index,js

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 //This is a calculator comment
+let count = 0
+const number = document.getElementById("number");
 
+const decreaseBtn = document.getElementById("decreaseBtn").onclick = function(){
+    count--;
+    number.textContent = count;
+}
+
+const increaseBtn = document.getElementById("increaseBtn").onclick = function(){
+    count++;
+    number.textContent = count;
+}
+
+const resetBtn = document.getElementById("resetBtn").onclick = function(){
+    count = 0;
+    number.textContent = count;
+}
 
