a4a135a89fe9689934bbd32f3314fccb037bb7de
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-08-22 16:59:05 +0100

    Added  a random number logic to index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-//This is a calculator comment
-let count = 0
+//This is a counter logic
+let count = 0;
 const number = document.getElementById("number");
 
 const decreaseBtn = document.getElementById("decreaseBtn").onclick = function(){
@@ -17,12 +17,54 @@ const resetBtn = document.getElementById("resetBtn").onclick = function(){
     number.textContent = count;
 }
 
-let max = 100;
-let min = 98;
+//This is a random number logic 
 
-let roundNumber = Math.round((Math.random() * (max - min)) + min);
+let newRandomNumber;
+const randomNumber = document.getElementById("randomNumber");
+const inputContainer = document.getElementById("inputContainer");
+const resetRanNumBtn = document.getElementById("resetRanNumBtn");
+const titleRandomNumber = document.getElementById('titleRandomNumber');
 
-console.log(roundNumber);
+function toggleInputVisibility(show) {
+    if (show) {
+        inputContainer.style.display = "flex";
+        resetRanNumBtn.style.display = "none";
+        titleRandomNumber.style.display = "flex";
+    } else {
+        inputContainer.style.display = "none";
+        resetRanNumBtn.style.display = "flex"; 
+        titleRandomNumber.style.display = "none";  
+    }
+}
+
+const createBtn = document.getElementById("createBtn").onclick = function(){
+    const minNumber = parseInt(document.getElementById("minNumber").value); 
+    const maxNumber = parseInt(document.getElementById("maxNumber").value);
+
+    if (!isNaN(minNumber) && !isNaN(maxNumber) && maxNumber > minNumber) {
+        newRandomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+        randomNumber.style.fontSize = "80px";
+        randomNumber.style.color = "#596e79";
+        randomNumber.textContent = newRandomNumber;
+        minNumber.textContent = "";
+        maxNumber.textContent = "";
+    } else {
+        randomNumber.style.fontSize = "16px";
+        randomNumber.style.color = "red";
+        randomNumber.textContent = "Invalid input!";
+        minNumber.textContent = "";
+        maxNumber.textContent = "";
+    }
+    toggleInputVisibility(false);
+}
+
+resetRanNumBtn.onclick = function(){
+    newRandomNumber = null;
+    randomNumber.textContent = "";
+    minNumber.textContent = "";
+    maxNumber.textContent = "";
+    toggleInputVisibility(true);
+}
 
 
 
