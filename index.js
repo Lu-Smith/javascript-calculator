f389dcd2c13b40b8d6b264f927ffb7b7ba3376db
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-03 16:38:38 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -117,6 +117,11 @@ function rollDice() {
     const diceImages = document.getElementById("diceImages");
     const values = [];
     const images = [];
+
+    for(let i = 0; i < numOfDice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        console.log(value);
+    }
 }
 
 
