dd6da831f7acd37d55369f2f374aae76f0e5a398
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-03 21:10:01 +0100

    Updated logic in index.js

--- index.js
+++ index.js
@@ -121,10 +121,11 @@ function rollDice() {
     for(let i = 0; i < numOfDice; i++) {
         const value = Math.floor(Math.random() * 6) + 1;
         values.push(value);
-        images.push(`<img src="./dice_images/Dice${value}.jpg" />`)
+        images.push(`<img src="./dice_images/Dice${value}.jpg" alt="Dice${value}" />`)
     }
 
-    diceImages.textContent = `dice: ${values.join(', ')}`;
+    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
 }
 
 
